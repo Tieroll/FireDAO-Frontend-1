95e2b2d55d61db7cd05f9f22311b0e75fe06da59
test <102033654+Tieroll@users.noreply.github.com>
2023-10-07 17:23:21 +0800

    Update userApi.js

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,15 +1,17 @@
-import $ from  'jquery'
-export  function  getSign(data){
+import $ from 'jquery'
+
+export function getSign(data) {
     return $.ajax({
-        type:"post",
-        url:'http://47.98.46.3:9528/index.php?action=sign' ,
-        data:data
+        type: "post",
+        url: 'http://47.98.46.3:9528/index.php?action=sign',
+        data: data
     })
 }
-export function daoRegister(data){
+
+export function daoRegister(data) {
     return $.ajax({
-        type:"post",
-        url:'http://47.98.46.3:9528/index.php?action=daoregister' ,
-        data:data
+        type: "post",
+        url: 'http://47.98.46.3:9528/index.php?action=daoregister',
+        data: data
     })
 }
