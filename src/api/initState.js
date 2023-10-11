f56c8db8e5e45db584c86e2022a73e63d39e136f
test <102033654+Tieroll@users.noreply.github.com>
2023-10-11 17:21:08 +0800

    Update initState.js

--- src/api/initState.js
+++ src/api/initState.js
@@ -2,14 +2,14 @@ module.exports = {
     api: null,
     apiError: null,
     apiState: null,
-    isShowNav:false,
-    account:"",
-    ethBalance:0,
-    myRecommender:"0x0000000000000000000000000000000000000000",
-    pid:undefined,
+    isShowNav: false,
+    account: "",
+    ethBalance: 0,
+    myRecommender: "0x0000000000000000000000000000000000000000",
+    pid: undefined,
     fid: 0,
-    fireSeedList:[],
-    PidArr:[],
-    networkId:null,
-    userInfo:{}
+    fireSeedList: [],
+    PidArr: [],
+    networkId: null,
+    userInfo: {}
 };
