ae2f3c2381fd037803fd1bf4b2a31479bc78187e
test <102033654+Tieroll@users.noreply.github.com>
2023-10-12 15:01:18 +0800

    Update getWeb3.js

--- src/api/getWeb3.js
+++ src/api/getWeb3.js
@@ -1,23 +1,22 @@
 import Web3 from "web3";
-
 let curProvider = null
-const getWeb3 = async (provider) => {
-    return new Promise(function (resolve, reject) {
-        try {
-            if (provider) {
+const getWeb3= async (provider)=>{
+    return new Promise(function (resolve,reject){
+        try{
+            if(provider){
                 curProvider = provider
-            } else if (window.ethereum) {
+            }else if (window.ethereum) {
                 let web3 = new Web3(window.ethereum)
                 curProvider = window.ethereum
                 resolve({
                     web3,
                     account: curProvider.accounts && curProvider.accounts.length > 0 ? curProvider.accounts[0] : null
                 })
-            } else {
+            }else{
                 reject(new Error("Unable to Connect"))
                 alert("Unable to Connect")
             }
-        } catch (e) {
+        }catch (e) {
             console.log(e)
             alert(e)
         }
